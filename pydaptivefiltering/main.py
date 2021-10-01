96e521bf3668aeb60f67865e01b24b2f0784b426
Bruno Lima <brunolimanetto@gmail.com>
2021-10-01 16:23:52 -0300

    Verbose and fixed NLMS and Newton_LMS

--- pydaptivefiltering/main.py
+++ pydaptivefiltering/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from time import time
 
 
 class AdaptiveFilter():
@@ -17,6 +18,14 @@ class AdaptiveFilter():
     adapt_LMS
     adapt_NLMS 
     adapt_Newton_LMS
+
+
+    filter_signal
+
+    Auxiliar Methods:
+    -----------------
+
+    _reset_history
 
     """
 
@@ -51,7 +60,7 @@ class AdaptiveFilter():
 
         return y
 
-    def adapt_LMS(self, desired_signal: np.ndarray, input_signal: np.ndarray, step: float = 1e-2) -> dict:
+    def adapt_LMS(self, desired_signal: np.ndarray, input_signal: np.ndarray, step: float = 1e-2, max_runs: int = 25,  tolerance=0, verbose=(True, 5)) -> dict:
         """
         Fit filter parameters to considering desired vector and inp
         ut x. desired and x must have length K,
@@ -66,6 +75,12 @@ class AdaptiveFilter():
         input signal to feed filter
         step : Convergence (relaxation) factor.
 
+        max_runs
+        max_iter
+
+        verbose (boolean, int): (True/False, Print_every)
+
+
         Outputs
         -------
 
@@ -79,36 +94,66 @@ class AdaptiveFilter():
         coefficients : numpy array
         Store the estimated coefficients for each iteration
         """
-        max_iter = desired_signal.size
+        verbose, print_every = verbose
+        # assert type(verbose) == bool
+        # assert type(print_every) == int && print_every > 0
 
-        x_k = np.zeros(self.filter_order+1, dtype=input_signal.dtype)
+        total_tic = time()
+        tic = time()
+        for run in range(max_runs):
 
-        errors_vector = np.array([])
-        outputs_vector = np.array([])
+            if verbose == True:
+                if run == max_runs - 1 or run % print_every == 0:
+                    print("Run {}\t ".format(run), end='')
 
-        for k in range(max_iter):
+            max_iter = desired_signal.size
 
-            x_k = np.concatenate(([input_signal[k]], x_k))[
-                :self.filter_order+1]
+            x_k = np.zeros(self.filter_order+1, dtype=input_signal.dtype)
 
-            w_k = self.coefficients
-            y_k = np.dot(w_k.conj(), x_k)
+            errors_vector = np.array([])
+            outputs_vector = np.array([])
 
-            error_k = desired_signal[k] - y_k
+            for k in range(max_iter):
 
-            next_w_k = w_k + step * error_k.conj() * x_k
+                x_k = np.concatenate(([input_signal[k]], x_k))[
+                    :self.filter_order+1]
 
-            errors_vector = np.append(errors_vector, error_k)
-            outputs_vector = np.append(outputs_vector, y_k)
-            #print(self.coefficients, [next_w_k])
-            self.coefficients = next_w_k
-            self.coefficients_history.append([next_w_k])
-        # updates the current filter coefficients
+                w_k = self.coefficients
+                y_k = np.dot(w_k.conj(), x_k)
 
+                error_k = desired_signal[k] - y_k
+
+                next_w_k = w_k + step * error_k.conj() * x_k
+
+                errors_vector = np.append(errors_vector, error_k)
+                outputs_vector = np.append(outputs_vector, y_k)
+
+                self.coefficients = next_w_k
+                self.coefficients_history.append([next_w_k])
+
+            if verbose == True:
+
+                if run == max_runs - 1 or print_every != -1 and run % print_every == 0:
+                    tac = time() - tic
+                    print('|error| = {:.02}\t Time: {:.03} ms'.format(
+                        np.abs(error_k), (tac)*1000))
+                    tic = time()
+            # tolerance break point
+
+            if np.abs(error_k) < tolerance:
+                if verbose == True:
+                    print(" ")
+                    print(" -- Ended at Run {} -- \n".format(run))
+                    print("Final |error| = {:.02}".format(np.abs(error_k)))
+                break
+
+        if verbose == True:
+            print(" ")
+            print('Total runtime {:.03} ms'.format((time() - total_tic)*1000))
         return {'outputs': outputs_vector,
-                'errors': errors_vector, 'coefficients': self.coefficients}
+                'errors': errors_vector, 'coefficients': self.coefficients_history}
 
-    def adapt_NLMS(self, desired_signal: np.ndarray, input_signal: np.ndarray, gamma: float, step: float = 1e-2) -> dict:
+    def adapt_NLMS(self, desired_signal: np.ndarray, input_signal: np.ndarray, gamma: float, step: float = 1e-2, max_runs: int = 25,  tolerance=0, verbose=(True, 5)) -> dict:
         """
         Fit filter parameters to considering desired vector and inp
         ut x. desired and x must have length K,
@@ -123,6 +168,12 @@ class AdaptiveFilter():
         input signal to feed filter
         step : Convergence (relaxation) factor.
 
+        max_runs
+        max_iter
+
+        verbose (boolean, int): (True/False, Print_every)
+
+
         Outputs
         -------
 
@@ -136,40 +187,68 @@ class AdaptiveFilter():
         coefficients : numpy array
         Store the estimated coefficients for each iteration
         """
-        max_iter = desired_signal.size
+        verbose, print_every = verbose
+        # assert type(verbose) == bool
+        # assert type(print_every) == int && print_every > 0
 
-        x_k = np.zeros(self.filter_order+1, dtype=input_signal.dtype)
+        total_tic = time()
+        tic = time()
+        for run in range(max_runs):
 
-        errors_vector = np.array([])
-        outputs_vector = np.array([])
+            if verbose == True:
+                if run == max_runs - 1 or run % print_every == 0:
+                    print("Run {}\t ".format(run), end='')
 
-        for k in range(max_iter):
+            max_iter = desired_signal.size
 
-            x_k = np.concatenate(([input_signal[k]], x_k))[
-                :self.filter_order+1]
+            x_k = np.zeros(self.filter_order+1, dtype=input_signal.dtype)
 
-            w_k = self.coefficients[k]
-            y_k = np.dot(w_k.conj(), x_k)
+            errors_vector = np.array([])
+            outputs_vector = np.array([])
 
-            error_k = desired_signal[k] - y_k
-            gamma_f = step/(x_k.conj()*x_k + gamma)
-            next_w_k = w_k + x_k * error_k.conj() * gamma_f
+            for k in range(max_iter):
 
-            next_w_k = w_k + step * error_k.conj() * x_k
+                x_k = np.concatenate(([input_signal[k]], x_k))[
+                    :self.filter_order+1]
 
-            errors_vector = np.append(errors_vector, error_k)
-            outputs_vector = np.append(outputs_vector, y_k)
-            self.coefficients = np.concatenate(
-                (self.coefficients, [next_w_k]))
+                w_k = self.coefficients
+                y_k = np.dot(w_k.conj(), x_k)
 
-        # updates the current filter coefficients
-        self.coefficients = w_k
-        self.coefficients_history.append([w_k])
+                error_k = desired_signal[k] - y_k
 
+                error_k = desired_signal[k] - y_k
+                gamma_f = step/(x_k.conj()*x_k + gamma)
+                next_w_k = w_k + x_k * error_k.conj() * gamma_f
+
+                errors_vector = np.append(errors_vector, error_k)
+                outputs_vector = np.append(outputs_vector, y_k)
+
+                self.coefficients = next_w_k
+                self.coefficients_history.append([next_w_k])
+
+            if verbose == True:
+
+                if run == max_runs - 1 or print_every != -1 and run % print_every == 0:
+                    tac = time() - tic
+                    print('|error| = {:.02}    Time: {:.03} ms'.format(
+                        np.abs(error_k), (tac)*1000))
+                    tic = time()
+            # tolerance break point
+
+            if np.abs(error_k) < tolerance:
+                if verbose == True:
+                    print(" ")
+                    print(" -- Ended at Run {} -- \n".format(run))
+                    print("Final |error| = {:.02}".format(np.abs(error_k)))
+                break
+
+        if verbose == True:
+            print(" ")
+            print('Total runtime {:.03} ms'.format((time() - total_tic)*1000))
         return {'outputs': outputs_vector,
-                'errors': errors_vector, 'coefficients': self.coefficients}
+                'errors': errors_vector, 'coefficients': self.coefficients_history}
 
-    def adapt_Newton_LMS(self, desired_signal: np.ndarray, input_signal: np.ndarray, alpha: float, initialInvRxHat: np.ndarray, step: float = 1e-2) -> dict:
+    def adapt_Newton_LMS(self, desired_signal: np.ndarray, input_signal: np.ndarray, alpha: float, initialInvRxHat: np.ndarray, step: float = 1e-2, max_runs: int = 25,  tolerance=0, verbose=(True, 5)) -> dict:
         """
         Fit filter parameters to considering desired vector and inp
         ut x. desired and x must have length K,
@@ -184,6 +263,12 @@ class AdaptiveFilter():
         input signal to feed filter
         step : Convergence (relaxation) factor.
 
+        max_runs
+        max_iter
+
+        verbose (boolean, int): (True/False, Print_every)
+
+
         Outputs
         -------
 
@@ -197,36 +282,69 @@ class AdaptiveFilter():
         coefficients : numpy array
         Store the estimated coefficients for each iteration
         """
-        max_iter = desired_signal.size
+        verbose, print_every = verbose
+        # assert type(verbose) == bool
+        # assert type(print_every) == int && print_every > 0
+        total_tic = time()
+        tic = time()
+        for run in range(max_runs):
 
-        x_k = np.zeros(self.filter_order+1, dtype=input_signal.dtype)
+            if verbose == True:
+                if run == max_runs - 1 or run % print_every == 0:
+                    print("Run {}\t ".format(run), end='')
 
-        errors_vector = np.array([])
-        outputs_vector = np.array([])
+            max_iter = desired_signal.size
 
-        for k in range(max_iter):
+            x_k = np.zeros(self.filter_order+1, dtype=input_signal.dtype)
 
-            x_k = np.concatenate(([input_signal[k]], x_k))[
-                :self.filter_order+1]
+            errors_vector = np.array([])
+            outputs_vector = np.array([])
+            invRxHat = initialInvRxHat
 
-            w_k = self.coefficients[k]
-            y_k = np.dot(w_k.conj(), x_k)
+            for k in range(max_iter):
 
-            error_k = desired_signal[k] - y_k
+                x_k = np.concatenate(([input_signal[k]], x_k))[
+                    :self.filter_order+1]
 
-            auxDen = (1-alpha)/alpha + x_k.conj()*invRxHat*x_k
-            invRxHat = (invRxHat-(invRxHat*x_k*x_k.conj()
-                                  * invRxHat)/auxDen)/(1 - alpha)
-            next_w_k = w_k + x_k * error_k.conj() * step * invRxHat
+                w_k = self.coefficients
+                y_k = np.dot(w_k.conj(), x_k)
 
-            errors_vector = np.append(errors_vector, error_k)
-            outputs_vector = np.append(outputs_vector, y_k)
-            self.coefficients = np.concatenate(
-                (self.coefficients, [next_w_k]))
+                error_k = desired_signal[k] - y_k
 
-        # updates the current filter coefficients
-        self.coefficients = w_k
-        self.coefficients_history.append([w_k])
+                auxDen = (1-alpha)/alpha + x_k.conj()*invRxHat*x_k
+                invRxHat = (invRxHat-(invRxHat*x_k*x_k.conj()
+                                      * invRxHat)/auxDen)/(1 - alpha)
 
+                next_w_k = w_k + x_k * error_k.conj() * step * invRxHat
+
+                errors_vector = np.append(errors_vector, error_k)
+                outputs_vector = np.append(outputs_vector, y_k)
+
+                self.coefficients = next_w_k
+                self.coefficients_history.append([next_w_k])
+
+            if verbose == True:
+
+                if run == max_runs - 1 or print_every != -1 and run % print_every == 0:
+                    tac = time() - tic
+                    print('|error| = {:.02}\t Time: {:.03} ms'.format(
+                        np.abs(error_k), (tac)*1000))
+                    tic = time()
+            # tolerance break point
+
+            if np.abs(error_k) < tolerance:
+                if verbose == True:
+                    print(" ")
+                    print(" -- Ended at Run {} -- \n".format(run))
+                    print("Final |error| = {:.02}".format(np.abs(error_k)))
+                break
+
+        if verbose == True:
+            print(" ")
+            print('Total runtime {:.03} ms'.format((time() - total_tic)*1000))
         return {'outputs': outputs_vector,
-                'errors': errors_vector, 'coefficients': self.coefficients}
+                'errors': errors_vector, 'coefficients': self.coefficients_history}
+
+    def _reset_history(self):
+        "Auxiliar function to reset coefficients_history"
+        self.coefficients_history = [self.coefficients]
