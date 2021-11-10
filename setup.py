7763d8ed422255f84f6bdcb7af88a29250759428
Bruno Lima <brunolimanetto@gmail.com>
2021-11-10 19:12:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pydaptivefiltering",
-    packages=["pydaptivefiltering"],
-    version='0.2',
+    packages=find_packages(
+        include=["pydaptivefiltering", "pydaptivefiltering.*"]),
+    version='0.3',
     description=" Python 3.8+ Implementation of the AdaptiveFiltering Toolbox.",
     author="Bruno Lima Netto",
     author_email="brunolimanetto@gmail.com",
     url="https://github.com/BruninLima",
-    download_url="https://github.com/BruninLima/PydaptiveFiltering/archive/refs/tags/v0.2.tar.gz",
+    download_url="https://github.com/BruninLima/PydaptiveFiltering/archive/refs/tags/v0.3.tar.gz",
     keywords=["Adaptive", "Filtering", "Digital", "Signal", "Processing"],
     install_requires=[
         'numpy',
