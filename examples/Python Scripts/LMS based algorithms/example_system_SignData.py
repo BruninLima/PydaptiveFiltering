871e1d2854c5cf49c65fe5c7104397c7c4e486f4
Bruno Lima <brunolimanetto@gmail.com>
2023-01-03 23:50:19 -0300

    Example Systems for SignData & SignError

--- examples/Python Scripts/LMS based algorithms/example_system_SignData.py
+++ examples/Python Scripts/LMS based algorithms/example_system_SignData.py
@@ -15,7 +15,7 @@
 # 3)  Choose an adaptive filtering algorithm to govern the rules of coefficient #
 #   updating.                                                                   #
 #                                                                               #
-#     Adaptive Algorithm used here: LMS                                         #
+#     Adaptive Algorithm used here: SignData                                    #
 #                                                                               #
 #################################################################################
 
@@ -25,20 +25,21 @@ import matplotlib.pyplot as plt
 import pydaptivefiltering as pdf
 
 
-def LMS_example():
+def SignData_example():
     """
+
     Types:
     ------
     None -> '(Filter, dictionary["outputs", "errors", "coefficients"])'
 
-    Example for the LMS algorithm, SignData and SignError. 
+    Example for the SignData algorithm. 
 
     """
     # Parameters
     K = 70                  # Number of iterations
-    H = np.array([0.32+0.21*1j, -0.3+0.7*1j, 0.5-0.8*1j, 0.2+0.5*1j])
+    H = np.array([0.32,-0.3,0.5,0.2])
     Wo = H                  # Unknown System
-    sigma_n2 = 0.04          # Noise Power
+    sigma_n2 = 0.04         # Noise Power
     N = 4                   # Number of coefficients of the adaptive filter
     mu = 0.1                # Convergence factor (step) (0 < μ < 1)
 
@@ -61,42 +62,41 @@ def LMS_example():
     # desired signal
     d = np.array(d) + n
 
-    # Instantiating Adaptive Filter
+    # Instantiating the adaptive filter
     Filter = pdf.AdaptiveFilter(W[:, 1])
-    print(" Adapting with LMS \n")
-    # Adapting with the LMS Algorithm
-    Output = pdf.LMS.LMS(Filter, d, x, mu)
+    print("Adapting with SignData algorithm")
+    # Adapting with SignData algorithm
+    Output = pdf.LMS.SignData(Filter, d, x, mu)
 
     return (Filter, Output, n)
 
 
 if __name__ == "__main__":
     # Running the model
-    Filter, Output, ComplexNoise = LMS_example()
+    Filter, Output, ComplexNoise = SignData_example()
 
     # Plotting the results
     plt.figure(figsize=(16, 16))
 
     plt.subplot(221)
-    plt.gca().set_title('Learning Curve for MSE [dB]')
-    MSE = [abs(err)**2 for err in Output['errors']]
+    plt.gca().set_title("Learning Curve for SignData [dB]")
+    MSE = [abs(err)**2 for err in Output["errors"]]
     plt.gca().semilogy(MSE)
     plt.grid()
     plt.subplot(222)
-    plt.gca().set_title('Learning Curve for MSEmin [dB]')
+    plt.gca().set_title("Learning Curve for MSEmin [dB]")
     MSEmin = [abs(n)**2 for n in ComplexNoise]
     plt.gca().semilogy(MSEmin)
     plt.grid()
     plt.subplot(223)
-    plt.gca().set_title('Evolution of the Coefficients (Real Part)')
-    real_part = [coef.real for coef in Output['coefficients']]
-    plt.gca().plot(real_part)
+    plt.gca().set_title("Evolution of the Coefficients (Real Part)")
+    plt.gca().plot(np.real(Output["coefficients"]))
     plt.grid()
     plt.subplot(224)
-    plt.gca().set_title('Evolution of the Coefficients (Imaginary Part)')
-    imag_part = [coef.imag for coef in Output['coefficients']]
-    plt.gca().plot(imag_part)
+    plt.gca().set_title("Evolution of the Coefficients (Imaginary Part)")
+    plt.gca().plot(np.imag(Output["coefficients"]))
     plt.grid()
-
     plt.tight_layout(pad=4.0)
     plt.show()
+
+# EOF
